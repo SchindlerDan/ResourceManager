3109f873abd5f0878a118dccba116179da4744ad
Daniel Schindler <schindld@arch10.cis.gvsu.edu>
2018-11-06 11:38:46 -0500

    Initial commit, resource and process classes written -Dan Schindler

--- Process.java
+++ Process.java
@@ -1,4 +1,60 @@
+import java.util.ArrayList;
 
 public class Process {
+	String name;
+	ArrayList<Resource> used;
+	ArrayList<Resource> desired;
 	
+	public Process(String pName) {
+		name = pName;
+		used = new ArrayList<>();
+		desired = new ArrayList<>();
+		
+	}
+	
+	public int releaseResource(Resource resource) {
+		for(int i = 0; i < used.size(); i++) {
+			if(used.get(i) == resource) {
+				used.remove(i);
+				System.out.println("Released resource " + resource.getName());
+				return 1;
+			}
+		}
+		return 0;
+	}
+	
+	public void addDesired(Resource resource) {
+		desired.add(resource);
+	}
+	
+	public int useResources() {
+		int newUses = 0;
+		for(int i = 0; i < desired.size(); i++) {
+			int success = desired.get(i).requestResource(this);
+			if(success > 0) {
+				used.add(desired.get(i));
+				System.out.println(name + " is now using resource " + desired.get(i).getName());
+				desired.remove(i);
+				newUses +=1;
+			}
+		}
+		System.out.println(name + " using " + newUses + "more resources");
+		return newUses;
+	}
+	
+	
+	public int getDesiredSize() {
+		return desired.size();
+	}
+	
+	public int getUsedSize() {
+		return used.size();
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+
+
 }
--- Resource.java
+++ Resource.java
@@ -1,4 +1,38 @@
 
 public class Resource {
+	private String name;
+	private Process user;
+	
+	public Resource(String pName) {
+		name = pName;
+	}
+	
+	public int requestResource(Process newUser) {
+		if(user == null) {
+			user = newUser;
+			return 1;
+		}
+		return 0;
+		
+	}
+	
+	public int freeResource() {
+		if(user == null) {
+			return 0;
+		}else {
+			user = null;
+			return 1;
+		}
+		
+	}
+	
+	public Process getUser() {
+		return user;
+	}
+	
+	public String getName() {
+		return name;
+		
+	}
 	
 }
